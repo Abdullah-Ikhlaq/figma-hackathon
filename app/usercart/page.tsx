c9fd9ef579900fd47fd2460ca4010542ae674ff8
Abdullah Ikhlaq <abdullahbiniklaq67@gmail.com>
2025-02-05 00:11:27 +0500

    Update

---
+++ app/usercart/page.tsx
@@ -0,0 +1,12 @@
+
+import UserCartComponent from "@/components/useCart/UserCartComponent";
+
+const UserCart = () => {
+  return (
+    <div className="bg-lightGray  relative mx-auto w-full xl:h-[1260px] h-auto overflow-hidden">
+      <UserCartComponent />
+    </div>
+  );
+};
+
+export default UserCart;
